94f3a3699c07402ed80cf7dde63cb9b06271a42c
Stephane Nguyen <snguyen@suse.com>
2025-11-08 17:59:24 +0100

    cleanup scripts

--- build.sh
+++ build.sh
@@ -2,14 +2,17 @@
 
 set -x
 
-DATE=$(/usr/bin/date +%Y%m%d%H%M)
+DATE=$(/usr/bin/date +%Y%m%d)
 SUM=$(echo ${DATE} | /usr/bin/md5sum)
-MACHINEID=${SUM%\ *}
+MACHINEID=${SUM% *}
 
 echo ${MACHINEID} > ./machine-id
 
+# Cleanup
+IMAGES_STEPH=$(podman image ls --format "{{.Id}}" -f reference=tumbleweed:steph)
+[[ -z ${IMAGES_STEPH} ]] || podman rmi -f ${IMAGES_STEPH%% *}
+
 podman build -t tumbleweed:${DATE} -f Containerfile.base .
-podman rm tumbleweed:steph
 podman tag tumbleweed:${DATE} tumbleweed:steph
 
 rm -f ./machine-id
