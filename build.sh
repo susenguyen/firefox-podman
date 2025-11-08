8ca62bb4258aece4ca32d1b3528c4471180337d7
Stephane Nguyen <snguyen@suse.com>
2025-11-08 16:15:47 +0100

    added fonts

--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
 #!/bin/env bash
 
+set -x
+
 DATE=$(/usr/bin/date +%Y%m%d%H%M)
 SUM=$(echo ${DATE} | /usr/bin/md5sum)
 MACHINEID=${SUM%\ *}
 
-echo ${MACHINEID} > /tmp/machine-id
+echo ${MACHINEID} > ./machine-id
 
 podman build -t tumbleweed:${DATE} -f Containerfile.base .
+podman rm tumbleweed:steph
+podman tag tumbleweed:${DATE} tumbleweed:steph
+
+rm -f ./machine-id
--- firefox-podman.sh
+++ firefox-podman.sh
@@ -5,6 +5,6 @@ PID=$$
 
 [[ -d /tmp/firefox-${PID} ]] || mkdir -p /tmp/firefox-${PID}
 
-podman run -dit -u steph -e DISPLAY=$DISPLAY -e XAUTHORITY=$XAUTHORITY -v /tmp/.X11-unix:/tmp/.X11-unix -v /run/user/1000:/run/user/1000 -v /tmp/firefox-${PID}:/home/steph/Downloads --security-opt label=disable --userns=keep-id:uid=1000,gid=1000 --name firefox tumbleweed:202511081231
+podman run -it -u steph -e DISPLAY=$DISPLAY -e XAUTHORITY=$XAUTHORITY -v /tmp/.X11-unix:/tmp/.X11-unix -v /run/user/1000:/run/user/1000 -v /tmp/firefox-${PID}:/home/steph/Downloads --security-opt label=disable --userns=keep-id:uid=1000,gid=1000 --name firefox --rm tumbleweed:steph
 
 # Intentionally not deleting /tmp/firefox-${PID} as /tmp is tmpfs on Tumbleweed
