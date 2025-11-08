fbd803f014786d39d9b977f7e36f98e8893af71d
Stephane Nguyen <snguyen@suse.com>
2025-11-08 14:28:57 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # firefox-podman
 Running Firefox in a container
+- No sound
+- No DRI
+- SElinux
---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/env bash
+
+DATE=$(/usr/bin/date +%Y%m%d%H%M)
+SUM=$(echo ${DATE} | /usr/bin/md5sum)
+MACHINEID=${SUM%\ *}
+
+echo ${MACHINEID} > ./machine-id
+
+podman build -t tumbleweed:${DATE} -f Containerfile.base .
