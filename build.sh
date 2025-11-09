eb01debb5d814fcb64112936a62b088da55bbb74
Stephane Nguyen <snguyen@suse.com>
2025-11-09 14:46:30 +0100

    fixed image cleanup

--- build.sh
+++ build.sh
@@ -9,8 +9,8 @@ MACHINEID=${SUM% *}
 echo ${MACHINEID} > ./machine-id
 
 # Cleanup
-IMAGES_STEPH=$(podman image ls --format "{{.Id}}" -f reference=tumbleweed:steph)
-[[ -z ${IMAGES_STEPH} ]] || podman rmi -f ${IMAGES_STEPH%% *}
+IMAGES_STEPH=$(podman image ls -q -f reference=tumbleweed:steph --no-trunc)
+[[ -z ${IMAGES_STEPH} ]] || podman rmi -f ${IMAGES_STEPH##*:}
 
 podman build -t tumbleweed:${DATE} -f Containerfile.base .
 podman tag tumbleweed:${DATE} tumbleweed:steph
